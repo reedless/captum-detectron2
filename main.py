217590d2e9042f580c245cca5a5ba7dc5bf8ba8d
Howard <howardyanghaowen@gmail.com>
2022-03-03 15:37:02 +0800

    try selecting one instance after forward pass

--- main.py
+++ main.py
@@ -20,75 +20,84 @@ model = build_model(cfg).eval()
 DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
 print("Model loaded")
 
-model = ModifiedGeneralizedRCNN(model)
+model = ModifiedGeneralizedRCNN(model).eval()
 
 
 # # TODO: convert outputs to scalar using wrapper...?
-# def wrapper(input, selected_pred=0):
-#       outputs = model(input)
-#       return outputs[0]['instances'][selected_pred].pred_classes[0]
+def wrapper(input, selected_pred=0):
+      outputs = model(input)
+      return outputs[0]['instances'][selected_pred].pred_classes[0]
 
 
 # define input and baseline
 input_   = torch.from_numpy(img).permute(2,0,1).unsqueeze(0)
 baseline = torch.zeros(input_.shape)
-# outputs = model(input_)
+
+# run input through model to get number of instances
+outputs = model(input_)
+
+print(outputs)
+
+for i in range(len(outputs[0]['instances'])):
+      print((f"Selecting instance prediction of "
+            f"class {outputs[0]['instances'][0].pred_classes[i]} with "
+            f"score {outputs[0]['instances'][0].scores[i]} probability."
+            ))
+
+      # Integrated Gradients
+      ig = IntegratedGradients(model)
+      attributions, delta = ig.attribute(input_, baseline, target=0, return_convergence_delta=True)
+      print('IG Attributions:', attributions)
+      print('Convergence Delta:', delta)
 
 
-# Integrated Gradients
-ig = IntegratedGradients(model)
-attributions, delta = ig.attribute(input_, baseline, target=0, return_convergence_delta=True)
-print('IG Attributions:', attributions)
-print('Convergence Delta:', delta)
+      # Gradient SHAP
+      gs = GradientShap(model)
+
+      # We define a distribution of baselines and draw `n_samples` from that
+      # distribution in order to estimate the expectations of gradients across all baselines
+      baseline_dist = torch.randn(5, 3, 480, 640) * 0.001
+      attributions, delta = gs.attribute(input_, stdevs=0.09, n_samples=4, baselines=baseline_dist,
+                                    target=0, return_convergence_delta=True)
+      print('GradientShap Attributions:', attributions)
+      print('Convergence Delta:', delta)
+      print('Average delta per example:', torch.mean(delta.reshape(input.shape[0], -1), dim=1))
 
 
-# Gradient SHAP
-gs = GradientShap(model)
-
-# We define a distribution of baselines and draw `n_samples` from that
-# distribution in order to estimate the expectations of gradients across all baselines
-baseline_dist = torch.randn(5, 3, 480, 640) * 0.001
-attributions, delta = gs.attribute(input_, stdevs=0.09, n_samples=4, baselines=baseline_dist,
-                                   target=0, return_convergence_delta=True)
-print('GradientShap Attributions:', attributions)
-print('Convergence Delta:', delta)
-print('Average delta per example:', torch.mean(delta.reshape(input.shape[0], -1), dim=1))
+      # Deep Lift
+      dl = DeepLift(model)
+      attributions, delta = dl.attribute(input_, baseline, target=0, return_convergence_delta=True)
+      print('DeepLift Attributions:', attributions)
+      print('Convergence Delta:', delta)
 
 
-# Deep Lift
-dl = DeepLift(model)
-attributions, delta = dl.attribute(input_, baseline, target=0, return_convergence_delta=True)
-print('DeepLift Attributions:', attributions)
-print('Convergence Delta:', delta)
+      # Deep Lift SHAP
+      dl = DeepLiftShap(model)
+      attributions, delta = dl.attribute(input_.float(), baseline_dist, target=0, return_convergence_delta=True)
+      print('DeepLiftSHAP Attributions:', attributions)
+      print('Convergence Delta:', delta)
+      print('Average delta per example:', torch.mean(delta.reshape(input.shape[0], -1), dim=1))
 
 
-# Deep Lift SHAP
-dl = DeepLiftShap(model)
-attributions, delta = dl.attribute(input_.float(), baseline_dist, target=0, return_convergence_delta=True)
-print('DeepLiftSHAP Attributions:', attributions)
-print('Convergence Delta:', delta)
-print('Average delta per example:', torch.mean(delta.reshape(input.shape[0], -1), dim=1))
+      # Noise Tunnel + Integrated Gradients
+      ig = IntegratedGradients(model)
+      nt = NoiseTunnel(ig)
+      attributions, delta = nt.attribute(input_, nt_type='smoothgrad', stdevs=0.02, nt_samples=4,
+            baselines=baseline, target=0, return_convergence_delta=True)
+      print('IG + SmoothGrad Attributions:', attributions)
+      print('Convergence Delta:', delta)
+      print('Average delta per example', torch.mean(delta.reshape(input.shape[0], -1), dim=1))
 
 
-# Noise Tunnel + Integrated Gradients
-ig = IntegratedGradients(model)
-nt = NoiseTunnel(ig)
-attributions, delta = nt.attribute(input_, nt_type='smoothgrad', stdevs=0.02, nt_samples=4,
-      baselines=baseline, target=0, return_convergence_delta=True)
-print('IG + SmoothGrad Attributions:', attributions)
-print('Convergence Delta:', delta)
-print('Average delta per example', torch.mean(delta.reshape(input.shape[0], -1), dim=1))
+      # Neuron Conductance
+      nc = NeuronConductance(model, model.backbone)
+      attributions = nc.attribute(input_, neuron_selector=1, target=0)
+      print('Neuron Attributions:', attributions)
 
 
-# Neuron Conductance
-nc = NeuronConductance(model, model.backbone)
-attributions = nc.attribute(input_, neuron_selector=1, target=0)
-print('Neuron Attributions:', attributions)
-
-
-# Layer Conductance
-lc = LayerConductance(model, model.backbone)
-attributions, delta = lc.attribute(input_, baselines=baseline, target=0, return_convergence_delta=True)
-print('Layer Attributions:', attributions)
-print('Convergence Delta:', delta)
+      # Layer Conductance
+      lc = LayerConductance(model, model.backbone)
+      attributions, delta = lc.attribute(input_, baselines=baseline, target=0, return_convergence_delta=True)
+      print('Layer Attributions:', attributions)
+      print('Convergence Delta:', delta)
 
--- modified_rcnn.py
+++ modified_rcnn.py
@@ -1,8 +1,10 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
+from detectron2.structures import Instances
 
 import torch
 from detectron2.modeling.meta_arch.rcnn import GeneralizedRCNN
 from detectron2.modeling.postprocessing import detector_postprocess
+from detectron2.utils.events import get_event_storage
 
 from modified_image_list import ModifiedImageList
 
@@ -79,6 +81,51 @@ class ModifiedGeneralizedRCNN(GeneralizedRCNN):
         losses.update(proposal_losses)
         return losses
 
+    def inference(
+        self,
+        batched_inputs: List[Dict[str, torch.Tensor]],
+        detected_instances: Optional[List[Instances]] = None,
+        do_postprocess: bool = True,
+    ):
+        """
+        Run inference on the given inputs.
+
+        Args:
+            batched_inputs (list[dict]): same as in :meth:`forward`
+            detected_instances (None or list[Instances]): if not None, it
+                contains an `Instances` object per image. The `Instances`
+                object contains "pred_boxes" and "pred_classes" which are
+                known boxes in the image.
+                The inference will then skip the detection of bounding boxes,
+                and only predict other per-ROI outputs.
+            do_postprocess (bool): whether to apply post-processing on the outputs.
+
+        Returns:
+            When do_postprocess=True, same as in :meth:`forward`.
+            Otherwise, a list[Instances] containing raw network outputs.
+        """
+        assert not self.training
+
+        images = self.preprocess_image(batched_inputs)
+        features = self.backbone(images.tensor)
+
+        if detected_instances is None:
+            if self.proposal_generator is not None:
+                proposals, _ = self.proposal_generator(images, features, None)
+            else:
+                assert "proposals" in batched_inputs[0]
+                proposals = [x["proposals"].to(self.device) for x in batched_inputs]
+
+            results, _ = self.roi_heads(images, features, proposals, None)
+        else:
+            detected_instances = [x.to(self.device) for x in detected_instances]
+            results = self.roi_heads.forward_with_given_boxes(features, detected_instances)
+
+        if do_postprocess:
+            assert not torch.jit.is_scripting(), "Scripting is not supported for postprocess."
+            return ModifiedGeneralizedRCNN._postprocess(results, batched_inputs, images.image_sizes)
+        else:
+            return results
 
     @staticmethod
     def _postprocess(instances, batched_inputs: List[Dict[str, torch.Tensor]], image_sizes):
