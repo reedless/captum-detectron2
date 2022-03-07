de37a1831e9b6c5810d863f0da36a1bc46f16b13
Howard <howardyanghaowen@gmail.com>
2022-03-07 15:34:56 +0800

    or None for optional args

--- modified_standard_roi_heads.py
+++ modified_standard_roi_heads.py
@@ -9,12 +9,12 @@ class ModifiedStandardROIHeads(StandardROIHeads):
                          box_pooler = standard_roi_heads_instance.box_pooler,
                          box_head = standard_roi_heads_instance.box_head,
                          box_predictor = ModifiedFastRCNNOutputLayers(standard_roi_heads_instance.box_predictor),
-                         mask_in_features = standard_roi_heads_instance.mask_in_features,
-                         mask_pooler = standard_roi_heads_instance.mask_pooler,
-                         mask_head = standard_roi_heads_instance.mask_head,
-                         keypoint_in_features = standard_roi_heads_instance.keypoint_in_features,
-                         keypoint_pooler = standard_roi_heads_instance.keypoint_pooler,
-                         keypoint_head = standard_roi_heads_instance.keypoint_head,
-                         train_on_pred_boxes = standard_roi_heads_instance.train_on_pred_boxes
+                         mask_in_features = standard_roi_heads_instance.mask_in_features or None,
+                         mask_pooler = standard_roi_heads_instance.mask_pooler or None,
+                         mask_head = standard_roi_heads_instance.mask_head or None,
+                         keypoint_in_features = standard_roi_heads_instance.keypoint_in_features or None,
+                         keypoint_pooler = standard_roi_heads_instance.keypoint_pooler or None,
+                         keypoint_head = standard_roi_heads_instance.keypoint_head or None,
+                         train_on_pred_boxes = standard_roi_heads_instance.train_on_pred_boxes or False,
                         )
 
