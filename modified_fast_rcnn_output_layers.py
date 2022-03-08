61faec7af26b74bbb9fb3389e5de23b393579950
Howard <howardyanghaowen@gmail.com>
2022-03-08 10:16:00 +0800

    only grad class_scores

--- modified_fast_rcnn_output_layers.py
+++ modified_fast_rcnn_output_layers.py
@@ -103,7 +103,7 @@ def fast_rcnn_inference_single_image(
     result = Instances(image_shape)
     result.pred_boxes = Boxes(boxes.detach())
     result.scores = scores.detach()
-    result.class_scores = class_scores.detach()
+    result.class_scores = class_scores
     result.pred_classes = filter_inds[:, 1].detach()
 
     return result, filter_inds[:, 0]
