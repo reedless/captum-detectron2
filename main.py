049fe42db5b274092524f28dbbb780b97d94b072
Howard <howardyanghaowen@gmail.com>
2022-04-19 16:15:30 +0800

    diff color

--- main.py
+++ main.py
@@ -119,10 +119,10 @@ for pred_class in outputs[0]['instances'].pred_classes.unique():
 
       fig, axs = plt.subplots(nrows=1, ncols=2, squeeze=False, figsize=(8, 8))
       axs[0, 0].set_title('Attribution mask')
-      axs[0, 0].imshow(attributions, cmap=plt.cm.inferno)
+      axs[0, 0].imshow(attributions, cmap='BuPu')
       axs[0, 0].axis('off')
       axs[0, 1].set_title('Overlay IG on Input image ')
-      axs[0, 1].imshow(attributions, cmap=plt.cm.inferno)
+      axs[0, 1].imshow(attributions, cmap='BuPu')
       axs[0, 1].imshow(img, alpha=0.5)
       axs[0, 1].axis('off')
       plt.tight_layout()
