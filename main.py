eea614129dd45b16b57403696caa3b375ae50cba
Howard <howardyanghaowen@gmail.com>
2022-04-21 17:42:50 +0800

    removed guided gradcam

--- main.py
+++ main.py
@@ -146,22 +146,22 @@ for pred_class in outputs[0]['instances'].pred_classes.unique():
       plt.savefig(f'GuidedBackprop_mask_{pred_class}.png', bbox_inches='tight') 
 
 
-      # GuidedGradCam
-      guided_gc = GuidedGradCam(wrapper, wrapper.model.roi_heads.box_predictor)
-      attribution = guided_gc.attribute(input_, target=pred_class)
+      # # GuidedGradCam
+      # guided_gc = GuidedGradCam(wrapper, wrapper.model.roi_heads.box_predictor)
+      # attribution = guided_gc.attribute(input_, target=pred_class)
 
-      attributions = attribution[0].permute(1,2,0).detach().cpu().numpy()
-      attributions = np.sum(np.abs(attributions), axis=-1)
+      # attributions = attribution[0].permute(1,2,0).detach().cpu().numpy()
+      # attributions = np.sum(np.abs(attributions), axis=-1)
 
-      print(np.sum(attributions), attributions.shape)
+      # print(np.sum(attributions), attributions.shape)
 
-      fig, axs = plt.subplots(nrows=1, ncols=2, squeeze=False, figsize=(8, 8))
-      axs[0, 0].set_title('Attribution mask')
-      axs[0, 0].imshow(attributions, cmap=plt.cm.inferno)
-      axs[0, 0].axis('off')
-      axs[0, 1].set_title('Overlay GuidedGradCam on Input image ')
-      axs[0, 1].imshow(attributions, cmap=plt.cm.inferno)
-      axs[0, 1].imshow(img, alpha=0.5)
-      axs[0, 1].axis('off')
-      plt.tight_layout()
-      plt.savefig(f'GuidedGradCam_mask_{pred_class}.png', bbox_inches='tight') 
+      # fig, axs = plt.subplots(nrows=1, ncols=2, squeeze=False, figsize=(8, 8))
+      # axs[0, 0].set_title('Attribution mask')
+      # axs[0, 0].imshow(attributions, cmap=plt.cm.inferno)
+      # axs[0, 0].axis('off')
+      # axs[0, 1].set_title('Overlay GuidedGradCam on Input image ')
+      # axs[0, 1].imshow(attributions, cmap=plt.cm.inferno)
+      # axs[0, 1].imshow(img, alpha=0.5)
+      # axs[0, 1].axis('off')
+      # plt.tight_layout()
+      # plt.savefig(f'GuidedGradCam_mask_{pred_class}.png', bbox_inches='tight') 
